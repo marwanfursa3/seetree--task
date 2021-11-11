115e74716fe60265b7d62c839a556f83abd51e6e
marwanfursa3 <91056519+marwanfursa3@users.noreply.github.com>
2021-11-11 02:17:31 +0200

    Update seetree.py

--- seetree.py
+++ seetree.py
@@ -1,32 +1,37 @@
-import Calculate_func
+    import Calculate_func
 from flask import Flask, render_template
 from PIL import Image, ImageOps
 import requests
 
 
 app = Flask(__name__)
-
+# dictionary data structure as data base to store the new functions requsets 
 multiple_requests = {"IMAGE_FILE_NAME": {"min": "0", "max": "0", "mean": "0", "median": "0"}}
 
+# rerroe request
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html")
 
-
+# home 
 @app.route('/', methods=['GET'])
 def HomePage():
     return render_template('home.html')
+# respond with “OK” to any request
 
 @app.route('/health', methods=['GET'])
 def health():
     return render_template('health.html')
 
+#calculate FUNC_NAME on the pixels of given IMAGE_FILE_NAME and return the result
 
 @app.route('/stats/<IMAGE_FILE_NAME>/<FUNC_NAME>', methods=['GET'])
 def get_url(IMAGE_FILE_NAME, FUNC_NAME):
     URL = 'https://storage.googleapis.com/seetree-demo-open/{}'.format(IMAGE_FILE_NAME)
+   # check the url image
     if not is_url_image(URL):
         return render_template('image-file-name-404.html')
+   # server gets a request it search the data base for identical request
     if IMAGE_FILE_NAME in multiple_requests:
         if FUNC_NAME in multiple_requests[IMAGE_FILE_NAME]:
             return render_template('Calculate-function.html', FUNC_NAME=FUNC_NAME, IMAGE_FILE_NAME=IMAGE_FILE_NAME,
@@ -34,18 +39,20 @@ def get_url(IMAGE_FILE_NAME, FUNC_NAME):
 
     input_image = Image.open(requests.get(URL, stream=True).raw)
     gray_image = ImageOps.grayscale(input_image)
-
+ # if the func_name is max return the max value and if not found the IMAGE_FILE_NAME ,FUNC_NAME updte the data base multiple_requests
     if FUNC_NAME == 'max':
         max_value = Calculate_func.Max_value(gray_image)
         multiple_requests.update({IMAGE_FILE_NAME: {FUNC_NAME: max_value}})
         multiple_requests[IMAGE_FILE_NAME].update({FUNC_NAME: max_value})
         return render_template('Calculate-function.html', FUNC_NAME=FUNC_NAME, IMAGE_FILE_NAME=IMAGE_FILE_NAME, value=max_value, url=URL )
+ # if the func_name is min return the min value and if not found the IMAGE_FILE_NAME ,FUNC_NAME updte the data base multiple_requests
 
     elif FUNC_NAME == 'min':
         min_value = Calculate_func.Min_value(gray_image)
         multiple_requests.update({IMAGE_FILE_NAME: {FUNC_NAME: min_value}})
         multiple_requests[IMAGE_FILE_NAME].update({FUNC_NAME: min_value})
         return render_template('Calculate-function.html', FUNC_NAME=FUNC_NAME, IMAGE_FILE_NAME=IMAGE_FILE_NAME, value=min_value, url=URL)
+ # if the func_name is mean return the mean value and if not found the IMAGE_FILE_NAME ,FUNC_NAME updte the data base multiple_requests
 
     elif FUNC_NAME == 'mean':
 
@@ -53,6 +60,7 @@ def get_url(IMAGE_FILE_NAME, FUNC_NAME):
         multiple_requests.update({IMAGE_FILE_NAME: {FUNC_NAME: mean_value}})
         multiple_requests[IMAGE_FILE_NAME].update({FUNC_NAME: mean_value})
         return render_template('Calculate-function.html', FUNC_NAME=FUNC_NAME, IMAGE_FILE_NAME=IMAGE_FILE_NAME,value=mean_value,url=URL)
+ # if the func_name is median return the median value and if not found the IMAGE_FILE_NAME ,FUNC_NAME updte the data base multiple_requests
 
     elif FUNC_NAME == 'median':
 
@@ -60,7 +68,7 @@ def get_url(IMAGE_FILE_NAME, FUNC_NAME):
         multiple_requests.update({IMAGE_FILE_NAME: {FUNC_NAME: median_value}})
         multiple_requests[IMAGE_FILE_NAME].update({FUNC_NAME: median_value})
         return render_template('Calculate-function.html', FUNC_NAME=FUNC_NAME, IMAGE_FILE_NAME=IMAGE_FILE_NAME,value=median_value,url=URL)
-
+# if the func_name is between p1 ... p100 return the pXXX value
     elif FUNC_NAME[0] == 'p' and FUNC_NAME[1:].isnumeric():
         if int(FUNC_NAME[1:]) >= 0 and int(FUNC_NAME[1:]) <= 100:
             percentile = Calculate_func.percentile_value(gray_image, int(FUNC_NAME[1:]))
@@ -74,7 +82,7 @@ def get_url(IMAGE_FILE_NAME, FUNC_NAME):
 
 
 
-
+# check if a URL to an image is up 
 def is_url_image(image_url):
     image_formats = ("image/png", "image/jpeg", "image/jpg")
     r = requests.head(image_url)
